8a8f04720d703a52cffee9d26e20e7402fb6401e
iraychev <ivoraychev@gmail.com>
2024-03-30 23:39:35 +0200

    added roles to user

--- src/main/java/com/iraychev/booking/model/User.java
+++ src/main/java/com/iraychev/booking/model/User.java
@@ -1,5 +1,6 @@
 package com.iraychev.booking.model;
 
+import com.iraychev.booking.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,6 +27,9 @@ public class User {
     @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "user_role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
     @Column(name = "last_name")
     private String lastName;
 
