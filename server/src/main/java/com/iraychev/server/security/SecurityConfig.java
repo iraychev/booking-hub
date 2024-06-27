23d6d917f155e8b81f9f942c60114590afa9684a
iraychev <ivoraychev1337@gmail.com>
2024-06-27 23:51:07 +0300

    fix allowed origin

--- server/src/main/java/com/iraychev/server/security/SecurityConfig.java
+++ server/src/main/java/com/iraychev/server/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig{
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.addAllowedOrigin("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
